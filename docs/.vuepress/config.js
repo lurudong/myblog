ca5bb32e819fa1f65437a0b30e5c069d000f41cf
dong <44517714+lurudong@users.noreply.github.com>
2023-06-29 15:14:59 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ export default{
 
   locales: {
     '/': {
-      lang: 'zh-CN',
+      lang: 'en-CN',
       title: 'NbCode',
       description: 'NbCode',
     },
@@ -17,58 +17,110 @@ export default{
 },
 
   theme: defaultTheme({
-    navbar: [
-      {
-        text: '首页',
-        link: '/',
-      },
-   
-      {
-        text: '后端',
-        link: '/dotnet/design-pattern.md',
-      },
-      {
-        text: '前端',
-        link: '/dotnet/design-pattern.md',
-      },
-      {
-        text: 'Linux运维/工具',
-        children: [
+
+    locales: {
+      /**
+       * English locale config
+       *
+       * As the default locale of @vuepress/theme-default is English,
+       * we don't need to set all of the locale fields
+       */
+      '/': {
+        // navbar
+        navbar: [
           {
-            text: 'MySql',
-            children:[
-              {
-                text:'MySql的安装',
-                link: '/liunx/mysql/Ubuntu 安装MySQL与配置及运程登录.md'
-              }
-             
-            ]
+            text: '首页',
+            link: '/',
+          },
+       
+          {
+            text: '后端',
+            link: '/dotnet/design-pattern.md',
           },
           {
-            text: 'Docker',
-            children:[
+            text: '前端',
+            link: '/dotnet/design-pattern.md',
+          },
+          {
+            text: 'Linux运维/工具',
+            children: [
               {
-                text:'Ubuntu安装Docker',
-                link: '/liunx/docker/Ubuntu下安装Docker.md'
-                
+                text: 'MySql',
+                children:[
+                  {
+                    text:'MySql的安装',
+                    link: '/liunx/mysql/Ubuntu 安装MySQL与配置及运程登录.md'
+                  }
+                 
+                ]
               },
               {
-                text:'Docker安装Redis',
-                link: '/liunx/docker/Dokcer下安装Redis容器.md'
-                
-              }
-             
+                text: 'Docker',
+                children:[
+                  {
+                    text:'Ubuntu安装Docker',
+                    link: '/liunx/docker/Ubuntu下安装Docker.md'
+                    
+                  },
+                  {
+                    text:'Docker安装Redis',
+                    link: '/liunx/docker/Dokcer下安装Redis容器.md'
+                    
+                  }
+                 
+                ]
+              },
             ]
           },
-        ]
+          {
+    
+            text: '程序员在线导航',
+            link: 'https://mouday.github.io/hao123'
+          }
+    
+        ],
+        selectLanguageName: '简体中文',
+        selectLanguageText: '选择语言',
+        selectLanguageAriaLabel: '选择语言',
+        // sidebar
+        // sidebar: sidebarZh,
+        // page meta
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdatedText: '上次更新',
+        contributorsText: '贡献者',
+        // custom containers
+        tip: '提示',
+        warning: '注意',
+        danger: '警告',
+        // 404 page
+        notFound: [
+          '这里什么都没有',
+          '我们怎么到这来了？',
+          '这是一个 404 页面',
+          '看起来我们进入了错误的链接',
+        ],
+        backToHome: '返回首页',
+        // a11y
+        openInNewWindow: '在新窗口打开',
+        toggleColorMode: '切换颜色模式',
+        toggleSidebar: '切换侧边栏',
+        
       },
-      {
 
-        text: '程序员在线导航',
-        link: 'https://mouday.github.io/hao123'
-      }
+      /**
+       * Chinese locale config
+       */
+      // '/en/': {
+      //   // navbar
+      //   navbar: navbarEn,
+      //   // sidebar
+      //   sidebar: sidebarEn,
+      //   // page meta
+      //   editLinkText: 'Edit this page on GitHub',
+      // },
+    },
 
-    ],
+   
   }),
  
   plugins: [
@@ -82,7 +134,7 @@ export default{
       // },
       locales: {
         '/': {
-          lang: 'zh-CN',
+          lang: 'en-CN',
           placeholder: '搜索文档',
           translations: {
             button: {
