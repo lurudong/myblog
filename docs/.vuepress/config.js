3cad95a67739e2cecadca78ca6ffc42d1db81475
dong <44517714+lurudong@users.noreply.github.com>
2023-06-29 15:43:39 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,7 +34,7 @@ export default{
           },
           {
             text: '测试',
-            link: '/test.md',
+            link: '/test.html',
           },
           {
             text: '后端',
